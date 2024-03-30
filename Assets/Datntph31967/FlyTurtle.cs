08219b8a3643c89b4fb376a365d66c92e2aec912
DatXinh <140891709+DatXinh@users.noreply.github.com>
2024-03-31 01:12:17 +0700

    FlyTurtle ver1.0
    Code cho con rùa bay qua lại giữa hai điểm và code animation

---
+++ Assets/Datntph31967/FlyTurtle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyTurtleTienDat : MonoBehaviour
+{
+    public Transform[] waypoint;
+    float moveSpeed = 5f;
+    int wayPointIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = waypoint[wayPointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlyTrutleMove();
+    }
+    void FlyTrutleMove()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, waypoint[wayPointIndex].position, moveSpeed * Time.deltaTime);
+        if (transform.position == waypoint[wayPointIndex].position)
+        {
+            wayPointIndex++;
+        }
+        if (wayPointIndex == waypoint.Length)
+        {
+            wayPointIndex = 0;
+        }
+    }
+}
